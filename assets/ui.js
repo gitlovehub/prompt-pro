05d762ba810b2b7426f5da94e609819045fa3d66
gitlovehub <hungtd010302@gmail.com>
2026-01-27 03:27:31 +0700

    Update ui.js

--- assets/ui.js
+++ assets/ui.js
@@ -104,7 +104,7 @@ export function renderPrompts({ gridEl, emptyStateEl, list, isAdmin }) {
             <button
               class="rounded-xl px-3 py-2 text-xs font-semibold text-red-600 ring-1 ring-red-200 hover:bg-red-50"
               data-action="delete">
-              Delete
+              Remove
             </button>
           `
                   : ``
